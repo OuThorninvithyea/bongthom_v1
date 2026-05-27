e75755ee86d504cd2bdeeac8415ea52dd4624a94
Chea <Vithyeass@gmail.com>
2026-05-27 22:49:29 +0700

    New changes

---
+++ admin/admin-api/pkg/http/response.go
@@ -0,0 +1,39 @@
+package http
+
+type Response struct {
+	Success    bool        `json:"success"`
+	Message    string      `json:"message"`
+	StatusCode int         `json:"status_code"`
+	Data       interface{} `json:"data"`
+}
+
+func NewResponse(message string, statusCode int, data interface{}) Response {
+	return Response{
+		Success:    true,
+		Message:    message,
+		StatusCode: statusCode,
+		Data:       data,
+	}
+}
+
+type ResponseWithPaging struct {
+	Success    bool        `json:"success"`
+	Message    string      `json:"message"`
+	StatusCode int         `json:"status_code"`
+	Data       interface{} `json:"data"`
+	Page       int         `json:"page"`
+	PerPage    int         `json:"per_page"`
+	Total      int         `json:"total"`
+}
+
+func NewResponseWithPaing(message string, statusCode int, data interface{}, page int, perpage int, total int) ResponseWithPaging {
+	return ResponseWithPaging{
+		Success:    true,
+		Message:    message,
+		StatusCode: statusCode,
+		Data:       data,
+		Page:       page,
+		PerPage:    perpage,
+		Total:      total,
+	}
+}
---
+++ admin/admin-api/pkg/logs/custom_log.go
@@ -0,0 +1,88 @@
+package logs
+
+import (
+	"fmt"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"runtime"
+)
+
+type CustomLog struct {
+	MessageID string
+	LogReason string
+	Function  string
+	File      string
+	Line      int
+}
+
+func (e *CustomLog) LogToString() string {
+	return fmt.Sprintf("MessageID: %s,LogReason:%s, Funtion: %s, File: %s, Line: %d",
+		e.MessageID, e.LogReason, e.Function, e.File, e.Line)
+}
+
+func NewCustomLog(messageID string, logDesc string, logType ...string) *CustomLog {
+	pc, file, line, ok := runtime.Caller(1)
+	function := "unknown"
+	if ok {
+		fn := runtime.FuncForPC(pc)
+		if fn != nil {
+			function = fn.Name()
+		}
+	}
+	msg := &CustomLog{
+		MessageID: messageID,
+		LogReason: logDesc,
+		Function:  function,
+		File:      file,
+		Line:      line,
+	}
+
+	switch {
+	case len(logType) > 0 && logType[0] == "fatal":
+		log.Fatal().Str("MessageID", msg.MessageID).
+			Str("LogReason", "logged : `"+msg.Function+"()`").
+			Str("Function", msg.Function).
+			Str("File", msg.File).
+			Int("Line", msg.Line).
+			Msg(msg.LogReason)
+	case len(logType) > 0 && logType[0] == "error":
+		log.Error().Str("MessageID", msg.MessageID).
+			Str("LogReason", "Logged : `"+msg.Function+"()`").
+			Str("Function", msg.Function).
+			Str("File", msg.File).
+			Int("Line", msg.Line).
+			Msg(msg.LogReason)
+	case len(logType) > 0 && logType[0] == "warn":
+		log.Warn().Str("MessageID", msg.MessageID).
+			Str("LogReason", "Logged : `"+msg.Function+"()`").
+			Str("Function", msg.Function).
+			Str("File", msg.File).
+			Int("Line", msg.Line).
+			Msg(msg.LogReason)
+	case len(logType) > 0 && logType[0] == "info":
+		log.Info().Str("MessageID", msg.MessageID).
+			Str("LogReason", "Logged : `"+msg.Function+"()`").
+			Str("Function", msg.Function).
+			Str("File", msg.File).
+			Int("Line", msg.Line).
+			Msg(msg.LogReason)
+	case len(logType) > 0 && logType[0] == "debug":
+		log.Debug().Str("MessageID", msg.MessageID).
+			Str("LogReason", "Logged : `"+msg.Function+"()`").
+			Str("Function", msg.Function).
+			Str("File", msg.File).
+			Int("Line", msg.Line).
+			Msg(msg.LogReason)
+	default:
+		if zerolog.GlobalLevel() >= zerolog.InfoLevel {
+			log.Info().Str("MessageID", msg.MessageID).
+				Str("LogReason", "Logged : `"+msg.Function+"()`").
+				Str("Function", msg.Function).
+				Str("File", msg.File).
+				Int("Line", msg.Line).
+				Msg(msg.LogReason)
+		}
+	}
+
+	return msg
+}
